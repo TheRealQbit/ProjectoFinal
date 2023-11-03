1bc2579b4421187b0bab3a0919c0d9c998311137
Qbit <65448292+TheRealQbit@users.noreply.github.com>
2023-11-03 13:13:31 +0100

    modo 10 sigue sin funcionar....

--- mode/mode10.c
+++ mode/mode10.c
@@ -1,6 +1,6 @@
 void mode10(dataCells** savedCells){
     for(int i = 0; i < getLastArrayElement(*savedCells); i++){
-                    printf("Cell %d: %s %s %s %d %s %d/%d %f GHz %d dBm \n",
-                   (*savedCells)[i].numCell, (*savedCells)[i].address, (*savedCells)[i].name, (*savedCells)[i].mode, (*savedCells)[i].ch, (*savedCells)[i].key, (*savedCells)[i].num1, (*savedCells)[i].num2, (*savedCells)[i].num3, (*savedCells)[i].frq);
+        printf("Cell %d: %s %s %s %d %s %d/%d %f GHz %d dBm \n",
+               (*savedCells)[i]->numCell, (*savedCells)[i]->address, (*savedCells)[i]->name, (*savedCells)[i]->mode, (*savedCells)[i]->ch, (*savedCells)[i]->key, (*savedCells)[i]->num1, (*savedCells)[i]->num2, (*savedCells)[i]->frq, (*savedCells)[i]->dbm);
     }
 }
